cbaf34a106f7377f352e264ae6c90028792d4e40
octavian.fusari <octavian.fusari@marconirovereto.it>
2023-12-30 15:43:35 +0100

    modifiche foto e css

--- src/app/components/ripetizioni/ripetizioni.component.ts
+++ src/app/components/ripetizioni/ripetizioni.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-ripetizioni',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./ripetizioni.component.css']
 })
 export class RipetizioniComponent {
-
+  constructor(private router: Router) { }
+ngOnInit(): void {
+  this.router.events.subscribe((event:any) => {
+    if (!(event instanceof NavigationEnd)) {
+      return;
+    }
+    window.scrollTo(0, 0)
+  });
+}
+scroll(el: HTMLElement) {
+  el.scrollIntoView({ behavior: 'smooth' });
+}
 }
